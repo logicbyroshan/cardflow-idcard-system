f2f06df9ec276577f74b24824f5e091f2c20c314
Adarsh ID <acw462011@gmail.com>
2026-04-11 12:47:01 +0530

    Set app identity to Adarsh Panel (in.adarshbhopal.panel1804)

--- mobile_shell_app/android/app/src/main/java/in/adarshbhopal/idcards/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package in.adarshbhopal.idcards;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- mobile_shell_app/capacitor.config.ts
+++ mobile_shell_app/capacitor.config.ts
@@ -3,8 +3,8 @@ import { config as loadEnv } from 'dotenv';
 
 loadEnv();
 
-const appName = process.env.APP_NAME || 'Adarsh ID Cards';
-const appId = process.env.APP_ID || 'in.adarshbhopal.idcards';
+const appName = process.env.APP_NAME || 'Adarsh Panel';
+const appId = process.env.APP_ID || 'in.adarshbhopal.panel1804';
 const pwaUrl = process.env.PWA_URL || 'https://panel.adarshbhopal.in/app/';
 const androidScheme = process.env.ANDROID_SCHEME || 'https';
 
